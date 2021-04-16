fb0b58fcf996946ddce92275c10620b955b19c75
Cody Mumford <cody.mumford@gmail.com>
2021-04-16 14:41:36 -0700

    Added test for getProductById

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,7 +1,17 @@
 const utils = require('../helpers/utils.js');
 
-test('utils.getProducts returns an array', () => {
+test('getProducts returns an array', () => {
   return utils.getProducts().then(data => {
     expect(data).toBeInstanceOf(Array);
+  });
+});
+
+test('getProductById returns an object when provided an id retrieved from getProducts()', () => {
+  return utils.getProducts()
+  .then(productList => {
+    return utils.getProductById(productList[0].id);
+  })
+  .then(product => {
+    expect(product).toBeInstanceOf(Object);
   });
 });
