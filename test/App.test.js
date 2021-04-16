63a282e53899966d054cfbe82b1d53e252775b85
nanaboat <p.nana.boateng@gmail.com>
2021-04-16 16:31:55 -0700

    Added test to App.test
    Added test for Ratings and Reviews
    Modified test to also check which components are
    rendered when the App components are rendered.

--- test/App.test.js
+++ test/App.test.js
@@ -3,10 +3,23 @@ import React from 'react';
 
 import App from '../client/src/App.jsx';
 import Banner from '../client/src/components/Banner.jsx';
+import RatingsAndReviews from '../client/src/components/RatingsAndReviews.jsx';
+import Overview from '../client/src/components/Overview.jsx';
+import QuestionsAnswers from '../client/src/components/QuestionsAnswers.jsx';
+import RelatedItems from '../client/src/components/RelatedItems.jsx';
 
-test("renders App component", () => {
+test ("renders App component", () => {
   const wrapper = shallow(<App />);
-  expect(wrapper.html()).toEqual('<div><header>Project Catwalk</header></div>');
+  expect(wrapper.contains(<Banner />)).toEqual(true);
+  expect(wrapper.contains(<Overview />)).toEqual(false);
+  expect(wrapper.contains(<RatingsAndReviews />)).toEqual(true);
+  expect(wrapper.contains(<QuestionsAnswers />)).toEqual(false);
+  expect(wrapper.contains(<RelatedItems />)).toEqual(false);
+});
+
+test("renders RatingsAndReviews component", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(RatingsAndReviews)).toHaveLength(1);
 });
 
 test("renders Banner component ", () => {
