db67cd109a329117965e3f9a48b941a4bd274f0d
Cody Mumford <cody.mumford@gmail.com>
2021-04-15 17:38:53 -0700

    https://trello.com/c/iKIKSFlz
    a product id is passed into each component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import utils from '../../helpers/utils.js';
+import sampleData from '../../helpers/sampleData.js';
+
 import Banner from './components/Banner.jsx';
 import Overview from './components/Overview.jsx';
 import Ratings from './components/Ratings.jsx';
@@ -8,19 +11,36 @@ import RelatedItems from './components/RelatedItems.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      products: sampleData.productList,
+      productId: sampleData.productList[0].id
+    }
+  }
+
+  componentDidMount() {
+    utils.getProducts()
+    .then(productList => {
+      this.setState({
+        products: productList,
+        productId: productList[0].id
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
   }
 
   render() {
+    let id = this.state.productId;
     return (
       <div>
         <Banner />
-        <Overview/>
-        <Ratings/>
-        <QuestionsAnswers/>
-        <RelatedItems/>
+        <Overview productId={id} />
+        <Ratings productId={id} />
+        <QuestionsAnswers productId={id} />
+        <RelatedItems productId={id} />
       </div>
-    )
-
+    );
   }
 }
 
