183985a3fda23b1308bfa81af497837e1e055ef6
nanaboat <p.nana.boateng@gmail.com>
2021-04-17 14:33:08 -0700

    Added drop down menu
    Added dropdown menu to SortOptions Component.
    Added handleChanges function that sets sortBy
    state to selected option in the dropdown menu

--- client/src/components/SortOptions.jsx
+++ client/src/components/SortOptions.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 
 
-const SortOptions = () => {
+const SortOptions = (props) => {
+  const {setSortBy} = props;
+
+  function handleChange(event) {
+    setSortBy(event.target.value);
+  };
+
   return (
     <div>
-      Sort Options
+      <p>
+        248 reviews, sorted by
+        <select name="sort-by" id="sort-by" onChange={handleChange}>
+          <option value='relevance'>Relevance</option>
+          <option value='helpful'>Helpful</option>
+          <option value='newest'>Newest</option>
+        </select>
+      </p>
     </div>
   );
 };
