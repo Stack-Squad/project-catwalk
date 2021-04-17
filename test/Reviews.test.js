75833456e76c504a1b5e1e550eb8a690e020942b
nanaboat <p.nana.boateng@gmail.com>
2021-04-17 10:46:46 -0700

    Remove console log
    Removed console.log line from this test.

--- test/Reviews.test.js
+++ test/Reviews.test.js
@@ -5,6 +5,5 @@ import Reviews from '../client/src/components/Reviews.jsx';
 
 test ("renders Reviews component", () => {
   const wrapper = shallow(<Reviews />);
-  console.log(wrapper.debug());
   expect(wrapper.contains(<h1>Reviews Component</h1>)).toEqual(true);
 });
