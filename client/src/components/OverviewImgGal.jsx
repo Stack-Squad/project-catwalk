4945a2d9693a6284e9867666530661ffd5a5353b
Ian Ferrier <ian.m.ferrier@gmail.com>
2021-04-19 14:03:07 -0700

    Overview -> Image Gallery -> Image rendering
    Getting some images rendered and testing layout.

--- client/src/components/OverviewImgGal.jsx
+++ client/src/components/OverviewImgGal.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from '../css-modules/overview-image-gallery.module.css';
 
-const OverviewImgGal = () => (
+const OverviewImgGal = (props) => (
   <div className={styles.imageGallery}>
-    <div className={styles.gallery}>Gallery</div>
-    <div className={styles.image}>Image</div>
+    <img src={props.currentSelectedStyleImages[0]['thumbnail_url']} className={styles.gallery}></img> {/* need to make dynamic render of all images*/}
+    <img src={props.currentImg} className={styles.image}></img>
     <div className={styles.switchImage}>Image Switch</div>
     <div className={styles.view}>View Switch</div>
   </div>
