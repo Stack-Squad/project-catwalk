93e34731cb7187d80c55cbbc7b7126b40ccc6113
Ian Ferrier <ian.m.ferrier@gmail.com>
2021-04-19 10:49:58 -0700

    Overview -> Component Refactoring
    Further refactor of Overview component.
    Adding a state, constuctor, and all that jazz.

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 class Overview extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      truth: true
+    }
+  }
   render() {
     return <div>Hello World</div>;
   }
