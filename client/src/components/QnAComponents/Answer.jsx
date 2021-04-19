5d2efb701b518c56ed065a73262018b5ed177d3f
Cody Mumford <cody.mumford@gmail.com>
2021-04-19 15:54:33 -0700

    added answerer and date to answer

--- client/src/components/QnAComponents/Answer.jsx
+++ client/src/components/QnAComponents/Answer.jsx
@@ -6,8 +6,15 @@ class Answer extends React.Component {
   }
 
   render() {
+    const answer = this.props.answer;
+    let answerer = answer.answerer_name;
+    let seller;
+    if (answerer === seller) {
+      answerer = <strong>Seller</strong>;
+    }
     return (<div>
-      {this.props.answer.body}
+      <div>{answer.body}</div>
+      <span>by {answerer}, {answer.date}</span>
     </div>);
   }
 }
