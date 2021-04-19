c5595c1d9d824ff9d065300c4be1f2f768e4dad4
Ian Ferrier <ian.m.ferrier@gmail.com>
2021-04-19 11:18:24 -0700

    Overview -> Image Gallery -> Subcomponent Start
    JSX file start for this subcomponent. Importing Image
    Gallery CSS properties into this jsx file.

---
+++ client/src/components/OverviewImgGal.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../css-modules/overview-image-gallery.module.css';
+
+const OverviewImgGal = () => (
+  <div className={styles.imageGallery}>
+
+  </div>
+);
+
+export default OverviewImgGal;
