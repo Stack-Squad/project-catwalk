46c884594b51e57462423213199826cdef6a48a5
Ian Ferrier <ian.m.ferrier@gmail.com>
2021-04-20 10:48:12 -0700

    Overview -> Image Gallery -> Next, Previous, Gallery Detect Current Image
    I have gone through some really fun CSS experiences and math here, but this is all being handled through a bunch of math logic, the function for next and previous handling is quite intense. Found a stackOverflow article for rounding up by five being used n line 57. Very crazy, I hope this handles the gallery five rendering correctly. Refactoring of this component will come later on style changes. But this component is almost done.

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -10,11 +10,13 @@ class Overview extends React.Component {
       data: sampleData.productStylesById.results, // just here for now, not useful yet, not sure if it will be
       currentImg: sampleData.productStylesById.results[0].photos[0].url, // the currently selected photo from currentSelectedStyleImages reference
       currentSelectedStyleImages: sampleData.productStylesById.results[0].photos, // reference from the current style's photos (CURRENTLY HARDCODED TO BE WORKING WITH ONE STYLE)
+      currentPointInGallery: 0,
       currentPointInGalleryStart: 0,
       currentPointInGalleryEndNonInclusive: 5,
       currentGalleryLength: sampleData.productStylesById.results[0].photos.length // length of currentSelectedStyleImages reference
     }
     this.galleryScrollClick = this.galleryScrollClick.bind(this);
+    this.nextAndPrevious = this.nextAndPrevious.bind(this);
   }
 
   galleryScrollClick(e) {
@@ -32,10 +34,39 @@ class Overview extends React.Component {
     });
   }
 
+  nextAndPrevious(e, option) {
+    console.log('clickity clicked: ', option);
+    if (this.state.currentPointInGallery < this.state.currentGalleryLength - 1 && option === 'next') {
+      this.state.currentPointInGallery += 1;
+      if (this.state.currentPointInGallery > this.state.currentPointInGalleryEndNonInclusive - 1) {
+        this.state.currentPointInGalleryStart += 5;
+        this.state.currentPointInGalleryEndNonInclusive += 5;
+      }
+    } else if (this.state.currentPointInGallery === this.state.currentGalleryLength - 1 && option === 'next') {
+      this.state.currentPointInGallery = 0;
+      this.state.currentPointInGalleryStart = 0;
+      this.state.currentPointInGalleryEndNonInclusive = 5;
+    } else if (this.state.currentPointInGallery > 0 && option === 'previous') {
+      this.state.currentPointInGallery -= 1;
+      if (this.state.currentPointInGallery < this.state.currentPointInGalleryStart) {
+        this.state.currentPointInGalleryStart -= 5;
+        this.state.currentPointInGalleryEndNonInclusive -= 5;
+      }
+    } else {
+      this.state.currentPointInGallery = this.state.currentGalleryLength - 1;
+      this.state.currentPointInGalleryEndNonInclusive = Math.ceil(this.state.currentGalleryLength / 5) * 5;
+      this.state.currentPointInGalleryStart = this.state.currentPointInGalleryEndNonInclusive - 5;
+    }
+    this.setState({
+      currentPointInGallery: this.state.currentPointInGallery,
+      currentImg: sampleData.productStylesById.results[0].photos[this.state.currentPointInGallery].url
+    });
+  }
+
   render() {
     return (
       <div>
-        <OverviewImgGal currentImg={this.state.currentImg} currentSelectedStyleImages={this.state.currentSelectedStyleImages} galleryScrollClick={this.galleryScrollClick} currentPointInGalleryStart={this.state.currentPointInGalleryStart} currentPointInGalleryEndNonInclusive={this.state.currentPointInGalleryEndNonInclusive} currentGalleryLength={this.state.currentGalleryLength} />
+        <OverviewImgGal currentImg={this.state.currentImg} currentSelectedStyleImages={this.state.currentSelectedStyleImages} galleryScrollClick={this.galleryScrollClick} currentPointInGalleryStart={this.state.currentPointInGalleryStart} currentPointInGalleryEndNonInclusive={this.state.currentPointInGalleryEndNonInclusive} currentGalleryLength={this.state.currentGalleryLength} nextAndPrevious={this.nextAndPrevious} currentPointInGallery={this.state.currentPointInGallery} />
       </div>
     )
   }
