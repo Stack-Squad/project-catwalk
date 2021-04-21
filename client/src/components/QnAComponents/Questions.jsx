a439f7d781f18eba015ec41d25ea88381dcceddc
Cody Mumford <cody.mumford@gmail.com>
2021-04-20 17:48:07 -0700

    button now renders two additional questions

--- client/src/components/QnAComponents/Questions.jsx
+++ client/src/components/QnAComponents/Questions.jsx
@@ -5,23 +5,43 @@ import sampleData from '../../../../helpers/sampleData.js';
 class Questions extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      fullQuestionList: [],
+      questionList: []
+    };
+    this.onClick = this.onClick.bind(this);
   }
 
-  render() {
+  onClick() {
+    console.log('clickety click');
+    this.setState({questionList: this.state.fullQuestionList.slice(0, this.state.questionList.length + 2)});
+  }
+
+  componentDidMount() {
     let questions = this.props.questions || sampleData.questionList.results;
     questions.sort((a, b) => {
       return b.question_helpfulness - a.question_helpfulness;
     });
-    const questionList = questions.map((question) =>
+    const fullQuestionList = questions.map((question) =>
       <div key={question.question_id}>
         <Question question={question} />
       </div>
     );
+    let questionList = fullQuestionList.slice(0, 4);
+    this.setState({
+      fullQuestionList: fullQuestionList,
+      questionList: questionList
+    });
+  }
 
+  render() {
+    const moreQuestionsButton = <button className='qa_button' onClick={this.onClick}>
+      More Answered Questions
+    </button>;
     return (<div className='questionscontainer'>
-      {questionList[0]}
-      {questionList[1]}
-      <button className='qa_button'>More Answered Questions</button><button className='qa_button'>Add a Question +</button>
+      {this.state.questionList}
+      {this.state.fullQuestionList.length > this.state.questionList.length && moreQuestionsButton}
+      <button className='qa_button'>Add a Question +</button>
     </div>);
   }
 }
