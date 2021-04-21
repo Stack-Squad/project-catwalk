97d7c6e9ce8733c6df9a85419ab0db5013faaacd
Cody Mumford <cody.mumford@gmail.com>
2021-04-21 11:33:39 -0700

    fixed linting issues

--- client/src/components/QnAComponents/Question.jsx
+++ client/src/components/QnAComponents/Question.jsx
@@ -1,44 +1,50 @@
 import React from 'react';
 
-import Answer from './Answer.jsx';
+import Answer from './Answer';
 
 class Question extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {};
   }
 
   render() {
-    const question = this.props.question;
+    const { question } = this.props;
 
-    let answers = [];
-    for (let id in question.answers) {
+    const answers = [];
+    for (const id in question.answers) {
       answers.push(question.answers[id]);
     }
-    answers.sort((a, b) => {
-      return b.helpfulness - a.helpfulness;
-    });
+    answers.sort((a, b) => b.helpfulness - a.helpfulness);
 
     let button;
     if (answers.length > 2) {
       button = <button>See More Answers</button>;
     }
 
-    const AnswerList = answers.map((answer) =>
-    <div key={answer.id}>
-      <Answer answer={answer} />
-    </div>
-    );
-
-    return (<div className='question_wrapper'>
-      <span className='qa_label' id='q_label'>Q:</span><span className='question_body'>{question.question_body}</span>
-      <span className='question_tags'>Helpful? Yes{`(${question.question_helpfulness})`} | Add Answer</span>
-      <span className='qa_label' id='a_label'>A:</span>
-      <div className='answer_list'>
-        {AnswerList[0]}
-        {AnswerList[1]}
-        {button}
+    const AnswerList = answers.map((answer) => (
+      <div key={answer.id}>
+        <Answer answer={answer} />
       </div>
-    </div>);
+    ));
+
+    const questionTag = `Helpful? Yes(${question.question_helpfulness}) | Add Answer`;
+
+    return (
+      <div className="question_wrapper">
+        <span className="qa_label" id="q_label">Q:</span>
+        <span className="question_body">{question.question_body}</span>
+        <span className="question_tags">
+          {questionTag}
+        </span>
+        <span className="qa_label" id="a_label">A:</span>
+        <div className="answer_list">
+          {AnswerList[0]}
+          {AnswerList[1]}
+          {button}
+        </div>
+      </div>
+    );
   }
 }
 
--- client/src/components/QnAComponents/Questions.jsx
+++ client/src/components/QnAComponents/Questions.jsx
@@ -1,47 +1,51 @@
 import React from 'react';
-import Question from './Question.jsx';
-import sampleData from '../../../../helpers/sampleData.js';
+import Question from './Question';
+import sampleData from '../../../../helpers/sampleData';
 
 class Questions extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       fullQuestionList: [],
-      questionList: []
+      questionList: [],
     };
     this.onClick = this.onClick.bind(this);
   }
 
-  onClick() {
-    this.setState({questionList: this.state.fullQuestionList.slice(0, this.state.questionList.length + 2)});
-  }
-
   componentDidMount() {
-    let questions = this.props.questions || sampleData.questionList.results;
-    questions.sort((a, b) => {
-      return b.question_helpfulness - a.question_helpfulness;
-    });
-    const fullQuestionList = questions.map((question) =>
+    const { questions } = this.props || sampleData.questionList.results;
+    questions.sort((a, b) => (b.question_helpfulness - a.question_helpfulness));
+    const fullQuestionList = questions.map((question) => (
       <div key={question.question_id}>
         <Question question={question} />
       </div>
-    );
-    let questionList = fullQuestionList.slice(0, 4);
+    ));
+    const questionList = fullQuestionList.slice(0, 4);
+    this.setState({ fullQuestionList, questionList });
+  }
+
+  onClick() {
+    const { questionList, fullQuestionList } = this.state;
     this.setState({
-      fullQuestionList: fullQuestionList,
-      questionList: questionList
+      questionList: fullQuestionList.slice(0, questionList.length + 2),
     });
   }
 
   render() {
-    const moreQuestionsButton = <button className='qa_button' onClick={this.onClick}>
-      More Answered Questions
-    </button>;
-    return (<div className='question_list_wrapper'>
-      {this.state.questionList}
-      {this.state.fullQuestionList.length > this.state.questionList.length && moreQuestionsButton}
-      <button className='qa_button'>Add a Question +</button>
-    </div>);
+    const moreQuestionsButton = (
+      <button className="qa_button" onClick={this.onClick}>
+        More Answered Questions
+      </button>
+    );
+    const { questionList, fullQuestionList } = this.state;
+
+    return (
+      <div className="question_list_wrapper">
+        {questionList}
+        {fullQuestionList.length > questionList.length && moreQuestionsButton}
+        <button className="qa_button">Add a Question +</button>
+      </div>
+    );
   }
 }
 
