c846930aff1b24559e87b69f3808fab6569bc183
nanaboat <p.nana.boateng@gmail.com>
2021-04-22 10:05:23 -0700

    Added id to Ratings and reviews Component
    This will help the overview component reference
    the ratings and reviews widget with an anchor tag.

--- client/src/components/RatingsAndReviews.jsx
+++ client/src/components/RatingsAndReviews.jsx
@@ -8,7 +8,7 @@ const RatingsAndReviews = (props) => {
   const [reviewData, setReviewData] = useState(sampleData.reviewMetaData);
   const [reviewList, setReviewList] = useState(sampleData.reviewList);
   return (
-    <div>
+    <div id="ratings-reviews">
       <h1>Ratings & Reviews</h1>
       <div className={styles.container}>
         <Ratings
