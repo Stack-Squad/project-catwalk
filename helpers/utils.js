77cd733acfc73a6d95cad163e89493e9b3552025
Cody Mumford <cody.mumford@gmail.com>
2021-04-22 11:26:25 -0700

    app now makes request to server for product list and an id is passed to each component

--- helpers/utils.js
+++ helpers/utils.js
@@ -4,39 +4,46 @@ const token = process.env.TOKEN;
 const baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp';
 
 module.exports.getProducts = () => {
-  let options = {
+  const options = {
     url: '/products',
     method: 'get',
-    baseURL: baseURL,
+    baseURL,
     headers: {
-      'Authorization': token
-    }
+      Authorization: token,
+    },
   };
 
   return axios(options)
-  .then(response => {
-    return response.data;
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .then((response) => response.data)
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 module.exports.getProductById = (id) => {
-  let options = {
+  const options = {
     url: `/products/${id}`,
     method: 'get',
-    baseURL: baseURL,
+    baseURL,
     headers: {
-      'Authorization': token
-    }
-  }
+      Authorization: token,
+    },
+  };
 
   return axios(options)
-  .then(response => {
-    return response.data;
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .then((response) => response.data)
+    .catch((err) => {
+      console.log(err);
+    });
 };
+
+// module.exports.getQuestionList = (id) => {
+//   const options = {
+//     url: `qa/questions?product_id=${id}`,
+//     method: 'get',
+//     baseURL,
+//     headers: {
+//       Authorization: token,
+//     },
+//   };
+// }
