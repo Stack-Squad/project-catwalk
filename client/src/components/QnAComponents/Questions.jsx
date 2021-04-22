14a473cb952c66354274dd9e6248d262b7b2d78a
Cody Mumford <cody.mumford@gmail.com>
2021-04-21 19:39:31 -0700

    Search bar now functional

--- client/src/components/QnAComponents/Questions.jsx
+++ client/src/components/QnAComponents/Questions.jsx
@@ -2,52 +2,27 @@ import React from 'react';
 import Question from './Question';
 import sampleData from '../../../../helpers/sampleData';
 
-class Questions extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fullQuestionList: [],
-      questionList: [],
-    };
-    this.onClick = this.onClick.bind(this);
-  }
+const Questions = ({ questions, fullList, onClick }) => {
+  questions.sort((a, b) => (b.question_helpfulness - a.question_helpfulness));
+  const listItems = questions.map((question) => (
+    <div key={question.question_id}>
+      <Question question={question} />
+    </div>
+  ));
 
-  componentDidMount() {
-    let { questions } = this.props;
-    if (!questions) { questions = sampleData.questionList.results; }
-    questions.sort((a, b) => (b.question_helpfulness - a.question_helpfulness));
-    const fullQuestionList = questions.map((question) => (
-      <div key={question.question_id}>
-        <Question question={question} />
-      </div>
-    ));
-    const questionList = fullQuestionList.slice(0, 4);
-    this.setState({ fullQuestionList, questionList });
-  }
+  const moreQuestionsButton = (
+    <button className="q_button" onClick={onClick}>
+      More Answered Questions
+    </button>
+  );
 
-  onClick() {
-    const { questionList, fullQuestionList } = this.state;
-    this.setState({
-      questionList: fullQuestionList.slice(0, questionList.length + 2),
-    });
-  }
-
-  render() {
-    const { questionList, fullQuestionList } = this.state;
-    const moreQuestionsButton = (
-      <button className="q_button" onClick={this.onClick}>
-        More Answered Questions
-      </button>
-    );
-
-    return (
-      <div className="question_list_wrapper">
-        {questionList}
-        {fullQuestionList.length > questionList.length && moreQuestionsButton}
-        <button className="qa_button">Add a Question +</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="question_list_wrapper">
+      {listItems}
+      {fullList.length > questions.length && moreQuestionsButton}
+      <button className="qa_button">Add a Question +</button>
+    </div>
+  );
+};
 
 export default Questions;
