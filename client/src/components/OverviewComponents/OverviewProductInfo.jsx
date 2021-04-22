76c7398d14436336256628959ce8644b301c0043
Ian Ferrier <ian.m.ferrier@gmail.com>
2021-04-21 17:25:51 -0700

    Implemented dummy locations for product info features on DOM.

--- client/src/components/OverviewComponents/OverviewProductInfo.jsx
+++ client/src/components/OverviewComponents/OverviewProductInfo.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 import styles from '../../css-modules/overview-product-info.module.css';
 
 const OverviewProductInfo = () => (
-  <div>Product Info Start</div>
+  <div className={styles.productInfoLayout}>
+    <div className={styles.rating}>Rating</div>
+    <div className={styles.category}>Category</div>
+    <div className={styles.name}>Name</div>
+    <div className={styles.price}>Price</div>
+    <div className={styles.share}>Share</div>
+  </div>
 );
 
 export default OverviewProductInfo;
+
+// "rating"
+// "category"
+// "name"
+// "price share";
