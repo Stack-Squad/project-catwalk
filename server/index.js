8586912804dd508ad3c77c37f3396b4386c2a06b
Cody Mumford <cody.mumford@gmail.com>
2021-04-22 15:12:01 -0700

    added path for request to mark question helpful

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.get('/products', (req, res) => {
 
 app.get('/products/:product_id', (req, res) => {
   const id = req.params.product_id;
-  console.log(`serving get request to /products/${id}`);
+  console.log(`serving GET request to /products/${id}`);
   utils.getProductById()
     .then((productInfo) => {
       res.json(productInfo);
@@ -32,10 +32,23 @@ app.get('/products/:product_id', (req, res) => {
 
 app.get('/qa/questions/:product_id', (req, res) => {
   const id = req.params.product_id;
-  console.log(`serving get request to /qa/questions/${id}`);
+  console.log(`serving GET request to /qa/questions/${id}`);
   utils.getQuestionList(id)
     .then((questionList) => {
       res.json(questionList);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.end();
+    });
+});
+
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  const id = req.params.question_id;
+  console.log(`serving PUT request to /qa/questions/${id}/helpful`);
+  utils.markQuestionHelpful(id)
+    .then((response) => {
+      res.send(response);
     })
     .catch((err) => {
       console.log(err);
