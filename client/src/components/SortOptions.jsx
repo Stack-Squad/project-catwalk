a0b73d2e5904499c0e24ae9ad52ab0aecec0a643
nanaboat <p.nana.boateng@gmail.com>
2021-04-23 10:43:55 -0700

    Render number of reviews
    Renders the number of reviews currently on page
    out of the total reviews.

--- client/src/components/SortOptions.jsx
+++ client/src/components/SortOptions.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import styles from '../css-modules/sortOptions.module.css';
 
 const SortOptions = (props) => {
-  const { setSortBy } = props;
+  const { setSortBy, reviewCount } = props;
+  const soFar = reviewCount[0];
+  const total = reviewCount[1];
 
   function handleChange(event) {
     setSortBy(event.target.value);
@@ -10,14 +12,37 @@ const SortOptions = (props) => {
 
   return (
     <div className={styles.container}>
-      <p>
-        248 reviews, sorted by
+      <div>
+        {soFar < total
+          ? (
+            <span>
+              Showing
+              {' '}
+              {soFar}
+              {' '}
+              out of
+              {' '}
+              {total}
+              {' '}
+              reviews
+            </span>
+          )
+          : (
+            <span>
+              Showing
+              {' '}
+              {total}
+              {' '}
+              reviews
+            </span>
+          ) }
+        <span>, sorted by</span>
         <select name="sort-by" id="sort-by" onChange={handleChange}>
           <option value="relevance">Relevance</option>
           <option value="helpful">Helpful</option>
           <option value="newest">Newest</option>
         </select>
-      </p>
+      </div>
     </div>
   );
 };
