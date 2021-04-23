94d76d52f68a295e2fd67a63b8470ecdc614c76a
nanaboat <p.nana.boateng@gmail.com>
2021-04-23 16:18:16 -0700

    Render data from api call
    Added a useEffect to fetch data from reviews api
    when component renders.

--- client/src/components/RatingsAndReviews.jsx
+++ client/src/components/RatingsAndReviews.jsx
@@ -1,14 +1,31 @@
 import React, { useState, useEffect } from 'react';
+
 import Ratings from './Ratings';
 import Reviews from './Reviews';
 import styles from '../css-modules/ratings-reviews.module.css';
 import sampleData from '../../../helpers/sampleData';
+import { getReviews, getReviewMetadata } from '../../../helpers/api';
 
 const RatingsAndReviews = (props) => {
   const { productId } = props;
-  const [reviewData, setReviewData] = useState(sampleData.reviewMetaData);
   const [reviewList, setReviewList] = useState(sampleData.reviewList.results);
-  const [reviews, setReviews] = useState([...reviewList]);
+  const [reviews, setReviews] = useState(sampleData.reviewList.results);
+  const [reviewData, setReviewData] = useState({ ...sampleData.reviewMetaData });
+
+  useEffect(() => {
+    getReviewMetadata(productId)
+      .then((reviewsMeta) => setReviewData({ ...reviewsMeta }))
+      .then(() => getReviews(productId))
+      .then((reviewsData) => {
+        setReviewList([...reviewsData]);
+        setReviews([...reviewsData]);
+      });
+    // const reviewsData = await getReviews(productId);
+    // const reviewsMeta = await getReviewMetadata(productId);
+    // setReviewList([...reviewsData]);
+    // setReviews([...reviewsData]);
+    // setReviewData({ ...reviewsMeta });
+  }, []);
 
   return (
     <div id="ratings-reviews">
