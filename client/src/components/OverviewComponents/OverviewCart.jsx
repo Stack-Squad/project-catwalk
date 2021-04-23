b4c6577fa88807efdb53e5f7706fa0f7567c18a8
Ian Ferrier <ian.m.ferrier@gmail.com>
2021-04-23 16:51:06 -0700

    Edit on defaults for dropdown menu.

--- client/src/components/OverviewComponents/OverviewCart.jsx
+++ client/src/components/OverviewComponents/OverviewCart.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const OverviewCart = () => (
   <form>
     <select>
-      <option value="testSize">testSize</option>
+      <option value="Select Size">Select Size</option>
     </select>
     <select>
-      <option value="testQuantity">testQuantity</option>
+      <option value="1">1</option>
     </select>
     <br />
     <input type="submit" value="ADD TO BAG" />
