7f4872ba14361186afffd227c729ab56bf341007
Cody Mumford <cody.mumford@gmail.com>
2021-04-23 16:00:32 -0700

    changed Questions to a class component

--- client/src/components/QnAComponents/Questions.jsx
+++ client/src/components/QnAComponents/Questions.jsx
@@ -2,27 +2,37 @@ import React from 'react';
 import Question from './Question';
 import sampleData from '../../../../helpers/sampleData';
 
-const Questions = ({ questions, fullList, onClick }) => {
-  questions.sort((a, b) => (b.question_helpfulness - a.question_helpfulness));
-  const listItems = questions.map((question) => (
-    <div key={question.question_id}>
-      <Question question={question} />
-    </div>
-  ));
+class Questions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-  const moreQuestionsButton = (
-    <button className="q_button" onClick={onClick}>
-      More Answered Questions
-    </button>
-  );
+    };
+  }
 
-  return (
-    <div className="question_list_wrapper">
-      {listItems}
-      {fullList.length > questions.length && moreQuestionsButton}
-      <button className="qa_button">Add a Question +</button>
-    </div>
-  );
-};
+  render() {
+    const { questions, fullList, onClick } = this.props;
+    questions.sort((a, b) => (b.question_helpfulness - a.question_helpfulness));
+    const listItems = questions.map((question) => (
+      <div key={question.question_id}>
+        <Question question={question} />
+      </div>
+    ));
+
+    const moreQuestionsButton = (
+      <button className="q_button" onClick={onClick}>
+        More Answered Questions
+      </button>
+    );
+
+    return (
+      <div className="question_list_wrapper">
+        {listItems}
+        {fullList.length > questions.length && moreQuestionsButton}
+        <button className="qa_button">Add a Question +</button>
+      </div>
+    );
+  }
+}
 
 export default Questions;
