9d8fb9924424cbd59728858d9dc124ed390890f3
nanaboat <p.nana.boateng@gmail.com>
2021-04-23 10:48:41 -0700

    unit tests
    Added unit tests and fixed broken tests.

--- test/Reviews.test.js
+++ test/Reviews.test.js
@@ -5,9 +5,11 @@ import Reviews from '../client/src/components/Reviews';
 import SortOptions from '../client/src/components/SortOptions';
 import ReviewsList from '../client/src/components/ReviewsList';
 import ReviewsButton from '../client/src/components/ReviewsButton';
+import sampleData from '../helpers/sampleData';
 
 test('renders Reviews component', () => {
-  const wrapper = shallow(<Reviews />);
+  const reviewList = sampleData.reviewList.results;
+  const wrapper = shallow(<Reviews reviewsList={reviewList} />);
   expect(wrapper.find(SortOptions)).toHaveLength(1);
   expect(wrapper.find(ReviewsList)).toHaveLength(1);
   expect(wrapper.find(ReviewsButton)).toHaveLength(1);
