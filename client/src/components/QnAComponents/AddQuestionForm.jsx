484f9f57ded90b156c1cead6657b10005504b796
Cody Mumford <cody.mumford@gmail.com>
2021-04-24 17:00:24 -0700

    added question form elements

--- client/src/components/QnAComponents/AddQuestionForm.jsx
+++ client/src/components/QnAComponents/AddQuestionForm.jsx
@@ -4,20 +4,52 @@ class AddQuestionForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      questionBody: '',
+      nickname: '',
+      email: '',
     };
+    this.handleQuestionChange = this.handleQuestionChange.bind(this);
+    this.handleNicknameChange = this.handleNicknameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleQuestionChange(e) {
+    this.setState({ questionBody: e.target.value });
+  }
+
+  handleNicknameChange(e) {
+    this.setState({ nickname: e.target.value });
+  }
+
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  handleSubmit(e) {
+
   }
 
   render() {
+    const { questionBody, nickname, email } = this.state;
     const { productName } = this.props;
 
     const title = 'Ask Your Question';
     const subtitle = `About the ${productName}`;
 
     return (
-      <div className="question-form">
+      <div className="question-form-wrapper">
         <div className="form-title" id="q-form-title">{title}</div>
         <div className="form-subtitle" id="q-form-subtitle">{subtitle}</div>
+        <form className="question-form" onSubmit={this.handleSubmit}>
+          <label htmlFor="question-body">Your Question*: </label>
+          <textarea id="question-body" name="fname" value={questionBody} onChange={this.handleQuestionChange} maxLength="100" required />
+          <label htmlFor="nickname">What is your nickname*: </label>
+          <input type="text" id="nickname" name="nickname" value={nickname} onChange={this.handleNicknameChange} required />
+          <label htmlFor="email">Your email*: </label>
+          <input type="text" id="email" name="email" value={email} onChange={this.handleEmailChange} required />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
