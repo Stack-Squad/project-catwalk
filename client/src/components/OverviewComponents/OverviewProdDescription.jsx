7fc69e8c1f9fe658d28172132da3a94471ea4325
Ian Ferrier <ian.m.ferrier@gmail.com>
2021-04-27 15:17:36 -0700

    Start of product description component

---
+++ client/src/components/OverviewComponents/OverviewProdDescription.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../../css-modules/overview-prod-description.module.css';
+
+const OverviewProdDescription = () => (
+  <div>Product Description</div>
+);
+
+export default OverviewProdDescription;
