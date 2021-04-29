ad590b0f3c5da0eecf7cfa5b5a9e01d91b2c9d3c
Cody Mumford <cody.mumford@gmail.com>
2021-04-29 14:16:47 -0700

    styling changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -46,7 +46,7 @@ class App extends React.Component {
   render() {
     const { productId, questionList, currentProduct } = this.state;
     return (
-      <div>
+      <div id="app">
         <Banner />
         <Overview productId={productId} />
         <RelatedItems productId={productId} />
